086887e690430195d95888226959fe0615d6d2da
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2021-08-03 16:43:19 +0100

    Fix the bug where dropping neutral sentiments

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     list_dirs = Helper.list_country_folders()
     sent = Sentiment()
     i = 0
-    Helper.printline(f"Iteraate through {len(list_dirs)} countries")
+    Helper.printline(f"Iterate through {len(list_dirs)} countries")
     for country in list_dirs:
         i += 1
         country_dir = get_country_dir(i, country)
@@ -30,8 +30,8 @@ def main():
         csv_input["s_neg"] = sent.neg
         csv_input["s_compound"] = sent.com
         csv_input["sentiment"] = sent.sent
-        csv_input = csv_input.drop(csv_input.query(f'sentiment=={sent.NEUTRAL}'))
-        Helper.printline(f"{i}. Country: {country} saving {len(csv_input)} entries")
+        csv_input = csv_input.drop(csv_input.query(f'sentiment=={sent.NEUTRAL}').index)
+        Helper.printline(f"Country: {i}. {country} saving {len(csv_input)} entries")
         csv_input.to_csv(Hyper.HyrdatedTweetFile)
     
     Helper.printline("    Finished")
